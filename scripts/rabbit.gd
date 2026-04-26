aa9ea7e6e683913759e179032ae7f8af5c249520
pedrokxz-sys <pedrogomes10lu@gmail.com>
2026-04-25 23:32:31 -0300

    rabbit

--- scripts/rabbit.gd
+++ scripts/rabbit.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 const jump_force = -200
 const speed = 100
 
-@onready var player_detector := $player_detector as RayCast2D
+@onready var player_detector := $player_detector as Area2D
 @onready var texture := $texture as Sprite2D
 @onready var animator := $animator as AnimationPlayer
 @onready var wall_detector := $wall_detector as RayCast2D
@@ -22,10 +22,10 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if player_detector.is_colliding() and !has_been_startle:
-		has_been_startle = true
-		animator.play("startle")
-		texture.scale.x = diretion * -1
+	#if !has_been_startle:
+		#has_been_startle = true
+		#animator.play("startle")
+		#texture.scale.x = diretion * -1
 
 	if animator.current_animation == "startle":
 		if is_on_floor():
@@ -46,3 +46,9 @@ func _physics_process(delta: float) -> void:
 func _on_animator_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "startle":
 		running = true
+
+func _on_player_detector_body_entered(body: Node2D) -> void:
+	if body !=self and !has_been_startle:
+		has_been_startle = true
+		animator.play("startle")
+		texture.scale.x = diretion * -1
